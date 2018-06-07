f419e272e46d8627f69eec4fdb16f64fdf6da328
John <634930172@qq.com>
2018-06-07 22:23:12 +0800

    设置忽略的文件并且提交2

--- app/src/main/java/com/john/testproject/activity/MainActivity.java
+++ app/src/main/java/com/john/testproject/activity/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends BaseAct {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //提交到git
+        //提交到git 2
     }
 
     /**
